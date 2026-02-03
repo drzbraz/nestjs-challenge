35a9fa39b286ba4996a5afe8223521c92b2ebb62
Daniel Braz <drz.braz@gmail.com>
2026-02-03 14:29:08 -0300

    chore: fix mongo url

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,6 +3,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const AppConfig = {
-  mongoUrl: process.env.MONGO_URL,
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/records',
   port: process.env.PORT || 3000,
 };
